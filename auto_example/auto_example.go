b2ec05dac2f428211668f9c4ce1a30a6427ad891
Laurent Demailly <ldemailly@gmail.com>
2025-03-12 20:44:48 -0700

    move the ugly AsyncCopy at the end of the file

--- auto_example/auto_example.go
+++ auto_example/auto_example.go
@@ -25,6 +25,51 @@ func main() {
 	os.Exit(Main())
 }
 
+func Main() int {
+	noAnsiFlag := flag.Bool("no-ansi", false, "Disable ANSI escape codes (colors and cursor movement)")
+	delayFlag := flag.Duration("delay", 5*time.Millisecond, "Artificially slowdown writes with this delay")
+	flag.Parse()
+	if flag.NArg() != 1 {
+		return usage()
+	}
+	url := flag.Arg(0)
+	fmt.Fprintf(os.Stderr, "Fetching %s\n", url)
+	cli := http.Client{}
+	req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, url, nil)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating request: %v\n", err)
+		return 1
+	}
+	req.Header.Set("Accept-Encoding", "identity")
+	resp, err := cli.Do(req) //nolint:bodyclose // closed by progressbar reader.Close() below.
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error fetching %s: %v\n", url, err)
+		return 1
+	}
+	rBar := progressbar.NewBarWithWriter(os.Stderr)
+	rBar.NoAnsi = *noAnsiFlag
+	rBar.Prefix = "R "
+	// On purpose different buffer size than the writer to show the effect of different speeds.
+	bufSize := 10 * 1024 * 1024 // 10MB
+	reader := progressbar.NewAutoReader(rBar, resp.Body, resp.ContentLength)
+	defer reader.Close()
+	wBar := progressbar.NewBarWithWriter(os.Stderr)
+	wBar.NoAnsi = *noAnsiFlag
+	wBar.Prefix = "W "
+	writer := progressbar.NewAutoWriter(wBar, bufio.NewWriterSize(os.Stdout, 16*1024), resp.ContentLength)
+	progressbar.MultiBar(0, rBar, wBar)
+	if resp.StatusCode != http.StatusOK {
+		fmt.Fprintf(os.Stderr, "Error fetching %s: %s\n", url, resp.Status)
+		return 1
+	}
+	err = AsyncCopy(writer, reader, bufSize, 100, *delayFlag)
+	writer.End()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error writing: %v\n", err)
+	}
+	return 0
+}
+
 // AsyncCopy is a fairly overly complicated replacement for io.Copy that decouples
 // reader and writer and optionally delays the writer.
 func AsyncCopy(dst io.Writer, src io.Reader, bufSize, chanSize int, delay time.Duration) error {
@@ -70,49 +115,4 @@ func AsyncCopy(dst io.Writer, src io.Reader, bufSize, chanSize int, delay time.D
 	default:
 		return nil
 	}
-}
-
-func Main() int {
-	noAnsiFlag := flag.Bool("no-ansi", false, "Disable ANSI escape codes (colors and cursor movement)")
-	delayFlag := flag.Duration("delay", 5*time.Millisecond, "Artificially slowdown writes with this delay")
-	flag.Parse()
-	if flag.NArg() != 1 {
-		return usage()
-	}
-	url := flag.Arg(0)
-	fmt.Fprintf(os.Stderr, "Fetching %s\n", url)
-	cli := http.Client{}
-	req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, url, nil)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error creating request: %v\n", err)
-		return 1
-	}
-	req.Header.Set("Accept-Encoding", "identity")
-	resp, err := cli.Do(req) //nolint:bodyclose // closed by progressbar reader.Close() below.
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error fetching %s: %v\n", url, err)
-		return 1
-	}
-	rBar := progressbar.NewBarWithWriter(os.Stderr)
-	rBar.NoAnsi = *noAnsiFlag
-	rBar.Prefix = "R "
-	// On purpose different buffer size than the writer to show the effect of different speeds.
-	bufSize := 10 * 1024 * 1024 // 10MB
-	reader := progressbar.NewAutoReader(rBar, resp.Body, resp.ContentLength)
-	defer reader.Close()
-	wBar := progressbar.NewBarWithWriter(os.Stderr)
-	wBar.NoAnsi = *noAnsiFlag
-	wBar.Prefix = "W "
-	writer := progressbar.NewAutoWriter(wBar, bufio.NewWriterSize(os.Stdout, 16*1024), resp.ContentLength)
-	progressbar.MultiBar(0, rBar, wBar)
-	if resp.StatusCode != http.StatusOK {
-		fmt.Fprintf(os.Stderr, "Error fetching %s: %s\n", url, resp.Status)
-		return 1
-	}
-	err = AsyncCopy(writer, reader, bufSize, 100, *delayFlag)
-	writer.End()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error writing: %v\n", err)
-	}
-	return 0
 }
